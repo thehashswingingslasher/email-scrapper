ec93e422014fdb90cab5c916a8262419550e7c6c
thehashswingingslasher <averrozen@gmail.com>
2024-11-27 00:31:24 +0700

    Update and rename email-scrapper.py to email_scrapper.py

--- email_scrapper.py
+++ email_scrapper.py
@@ -11,15 +11,21 @@ emails = set()
 count = 0
 limit = int(input('[+] Enter search limit: '))
 
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
 
 try:
-    while True:
+    while urls:
         count += 1
         if count > limit:
             break
 
         url = urls.popleft()
+        if url in scraped_urls:
+            continue
         scraped_urls.add(url)
+        
         parts = urllib.parse.urlsplit(url)
         base_url = f'{parts.scheme}://{parts.netloc}'
         path = url[:url.rfind('/') + 1] if '/' in parts.path else url  
@@ -27,14 +33,16 @@ try:
         print(f'{count} Processing {url}')
 
         try:
-            response = requests.get(url)
-        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):  
+            response = requests.get(url, headers=headers)
+            response.raise_for_status()  # Raise exception for bad responses
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.HTTPError):  
             continue
 
-        new_emails = set(re.findall(r'[a-z0-9\.\-+_]+@\w\.+[a-z\.]+', response.text, re.I))
+        new_emails = set(re.findall(r'[a-z0-9\.\-+_]+@[a-z0-9\.-]+\.[a-z]{2,}', response.text, re.I))
         emails.update(new_emails)
 
         soup = BeautifulSoup(response.text, 'html.parser')
+
         for anchor in soup.find_all('a'):
             link = anchor.attrs['href'] if 'href' in anchor.attrs else ''
             if link.startswith('/'):
